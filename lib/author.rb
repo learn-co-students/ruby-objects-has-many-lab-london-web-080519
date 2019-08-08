6232f3913e47bb06cd35578134f6cf25db9bdd76
Sohaib <sohaibmh@yahoo.com>
2019-08-08 18:55:49 +0100

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author = self}
+  end
+
+  def author_name(post)
+    post.author ? post.author.name : nil
+  end
+
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    add_post(new_post)
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+end
---
+++ lib/run.rb
@@ -0,0 +1,6 @@
+require "pry"
+require_relative "song.rb"
+require_relative "artist.rb"
+
+
+binding.pry
