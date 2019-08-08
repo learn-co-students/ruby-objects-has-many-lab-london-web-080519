38569060b46fc07fffc28745dd257e557847cc14
oliburt <oliburt@gmail.com>
2019-08-08 08:45:42 +0100

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,27 @@
+class Artist
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def songs
+        Song.all.each{|song| @songs << song if song.artist == self}
+        @songs
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(name)
+        song = Song.new(name)
+        self.add_song(song)
+    end
+
+    def self.song_count
+        Song.all.count
+    end
+
+end
